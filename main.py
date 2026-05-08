c8e890f5f595da8dee526e005d7fb54ec15bdc02
Eng-Ahmed-Farouk-backup <ahmed.farouk@adapt-eg.org>
2026-05-09 00:54:24 +0300

    Add comments Explain every single line
    Refactor calc function and improve comments for clarity

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
-def calc (x,y,s) : 
-    result = 0 
-    if s == "+" :
-        result = x+y
-    elif s == "/" :
-        result = x/y
-    elif s == "*" :
-        result = x*y
-    elif s == "-" :
-        result = x-y
-    if result % 2 == 0 and result != 0:
-        print (result,"is even")
-    elif result == 0 :
-        print("It's zero !!!!!!!!!!!!!!!!!!!!")
-    else :
-        print (result,"is odd")
-    return result
-def q(s):
-    x = int (input ("Enter the frist Number : "))
-    y = int (input ("Enter the second Number : "))
-    print ("the Value = ",calc (x,y,s)) 
-    return s
+def calc (x,y,s) : # Function of the operations
+    result = 0 # Store result Var
+    if s == "+" : # check if the user choosed +
+        result = x+y # get the sum of the numbers and store it in result var
+    elif s == "/" : # check if the user choosed /
+        result = x/y # get the result of the numbers and store it in result var
+    elif s == "*" : # check if the user choosed *
+        result = x*y # get the result of the numbers and store it in result var
+    elif s == "-" : # check if the user choosed -
+        result = x-y # get the result of the numbers and store it in result var
+    if result % 2 == 0 and result != 0: # Check if the number was Even 
+        print (result,"is even") # Say the number is even
+    elif result == 0 : # check if it's zero
+        print("It's zero !!!!!!!!!!!!!!!!!!!!") # print it's zero !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    else : # check if it something else (odd number)
+        print (result,"is odd") # print it is odd
+    return result # get the Result Value
+def q(s): # function of the result
+    x = int (input ("Enter the frist Number : ")) # get the first number
+    y = int (input ("Enter the second Number : ")) # get the second one
+    print ("the Value = ",calc (x,y,s)) # print the value 
+    return s # return the s var
 
-while True:
-    s = input("Enter the opretion Symbol (/,*,+,-) or Enter 0 to exit: ")
-    if s == "0" :
-        break
-    else :
-        q(s)
+while True: # loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooop
+    s = input("Enter the opretion Symbol (/,*,+,-) or Enter 0 to exit: ") # check the operation symbol that the user wants I hat the user
+    if s == "0" : # check if it zero
+        break # close the app yayyyy
+    else : # else
+        q(s) # nana ur buessines 
+# lol lmao lmfao :ok-cry: :joy: :crydeath:
